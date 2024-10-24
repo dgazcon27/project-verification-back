93f44f48ef697b9760935625f550cf11ed5a8a5b
Daniel Gazcon <daniel.gazcon27@gmail.com>
2024-10-24 14:19:42 -0400

    Save image reference

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var { upload } = require('./middleware/uploadImage')
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', upload, usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
